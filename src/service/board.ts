7d4f518b81622d40f8f6e4228d034c67b8e2bad1
1tang2bang92 <shpark0906@naver.com>
2023-05-18 00:48:40 +0900

    테스트 데이터 저장하고있는 서비스

---
+++ src/service/board.ts
@@ -0,0 +1,16 @@
+/**
+ * BOARD
+ * UID: int
+ * TITLE: string
+ * BODY: string | XML
+ * UPDATE_AT: datetime
+ */
+
+const getPosts = () => {
+    return {
+        'uid': 1,
+        'title': 'hello',
+        'body': 'world!',
+        'update_at': 'time'
+    }
+}
---
+++ src/service/profile.ts
@@ -0,0 +1,8 @@
+export const getProfile = () => {
+    return {
+        'username': '1tang2bang92',
+        'github': 'https://github.com/1tang2bang92',
+        'youtube': 'https://www.youtube.com/c/1tang2bang92',
+        'naverBlog': 'https://blog.naver.com/shpark0906'
+    }
+}
