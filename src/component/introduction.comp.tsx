42f7502f0ec5bf611f80debe54c1d3e5000a4efa
1tang2bang92 <shpark0906@naver.com>
2023-05-20 04:36:11 +0900

    아이콘 정렬 및 최적화

--- src/component/introduction.comp.tsx
+++ src/component/introduction.comp.tsx
@@ -1,40 +1,58 @@
-import { useRecoilValueLoadable } from "recoil";
-import { Paper, styled } from "@mui/material";
-import { profileSelector } from "../recoil/selector/profile.selector";
+import { useRecoilValueLoadable } from "recoil"
+import { Paper } from "@mui/material"
+import styled from '@emotion/styled'
+import { profileSelector } from "../recoil/selector/profile.selector"
+import { useEffect, useState, memo } from "react"
 
 const IntroductionComp = () => {
-  const profile = useRecoilValueLoadable(profileSelector);
-  const { contents } = profile;
-  const { img, txt } = contents;
+  const profile = useRecoilValueLoadable(profileSelector)
+  const { contents } = profile
+  const { img, txt } = contents
+  const [renderedContent, setRenderedContent] = useState(null)
 
-  const renderContent = () => {
-    const links = Object.entries(txt).filter(([site]) => site !== "username");
-    return (
-      <>
-        <img src={img} height={100} />
-        <p>{txt.username}</p>
-        {links.map(([site, url]: any) => (
-          <a key={site} href={url}>{site}</a>
-        ))}
-      </>
-    );
-  };
+  useEffect(() => {
+    const renderContent = (): any => {
+        const links = Object.entries(txt).filter(([site]) => site !== "username")
+        return (
+            <>
+                <img src={img} height={100} />
+                <p>{txt.username}</p>
+                <IconsWarpper>
+                    {links.map(([site, url]: any) => (
+                    <a key={site} href={url} about={site}>
+                        ㅁ
+                    </a>
+                    ))}
+                </IconsWarpper>
+            </>
+        )
+    }
+    
+    if (profile.state === "hasValue") {
+      setRenderedContent(renderContent())
+    }
+  }, [profile.state, contents])
 
   return (
     <MainFrame elevation={10}> {{
-            hasValue: renderContent(),
+            hasValue: renderedContent,
             loading:  <b>Loading...</b>,
             hasError: <b>Error</b>,
         }[profile.state]
     }</MainFrame>
-  );
-};
+  )
+}
 
 const MainFrame = styled(Paper)`
   display: flex;
   flex-direction: column;
   width: 200px;
   align-items: center;
-`;
+`
+const IconsWarpper = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+`
 
-export default IntroductionComp;
+export default memo(IntroductionComp)
