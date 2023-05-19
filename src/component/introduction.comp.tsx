e787c9ab6f38e0e1ae0417808582a448f9af2f91
1tang2bang92 <shpark0906@naver.com>
2023-05-20 04:16:35 +0900

    비동기 호출을 recoil loadable로 변경

--- src/component/introduction.comp.tsx
+++ src/component/introduction.comp.tsx
@@ -1,30 +1,40 @@
-import { useRecoilValueLoadable } from "recoil"
-import { Suspense } from 'react'
-
-import { Paper, styled } from "@mui/material"
-
-import { profileSelector } from "../recoil/selector/profile.selector" 
+import { useRecoilValueLoadable } from "recoil";
+import { Paper, styled } from "@mui/material";
+import { profileSelector } from "../recoil/selector/profile.selector";
 
 const IntroductionComp = () => {
+  const profile = useRecoilValueLoadable(profileSelector);
+  const { contents } = profile;
+  const { img, txt } = contents;
 
-    const profile = useRecoilValueLoadable(profileSelector);
-
+  const renderContent = () => {
+    const links = Object.entries(txt).filter(([site]) => site !== "username");
     return (
-        <MainFrame elevation={10}>
-            <Suspense fallback={<b> Loading... </b>}>
-                <img src={ profile.contents.img } height={100}/>
-                <p> {profile.contents.txt.username} </p>
-                <p> {JSON.stringify(profile.contents.txt)} </p>
-            </Suspense>
-        </MainFrame>
-    )
-}
+      <>
+        <img src={img} height={100} />
+        <p>{txt.username}</p>
+        {links.map(([site, url]: any) => (
+          <a key={site} href={url}>{site}</a>
+        ))}
+      </>
+    );
+  };
+
+  return (
+    <MainFrame elevation={10}> {{
+            hasValue: renderContent(),
+            loading:  <b>Loading...</b>,
+            hasError: <b>Error</b>,
+        }[profile.state]
+    }</MainFrame>
+  );
+};
 
 const MainFrame = styled(Paper)`
-    display: flex;
-    flex-direction: column;
-    width: 200px;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  align-items: center;
+`;
 
-export default IntroductionComp
+export default IntroductionComp;
