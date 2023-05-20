488e78b0c3596cbb88e9853cfee06a0e97c1fc43
1tang2bang92 <shpark0906@naver.com>
2023-05-20 17:17:49 +0900

    node를 직접 return하게 확장자 변경

---
+++ src/service/profile.tsx
@@ -0,0 +1,31 @@
+import LanguageIcon from '@mui/icons-material/Language'
+import Blog from '../assets/naver_blog_icon.png'
+import YouTubeIcon from '@mui/icons-material/YouTube'
+import GitHubIcon from '@mui/icons-material/GitHub'
+
+
+export const getProfile = () => {
+  return {
+    username: '1tang2bang92',
+    github: 'https://github.com/1tang2bang92',
+    youtube: 'https://www.youtube.com/c/1tang2bang92',
+    naverBlog: 'https://blog.naver.com/shpark0906',
+  }
+}
+
+export const getProfileMsg = () => {
+    return "Hello world!"
+}
+
+export const getIcons = (site: string) => {
+  switch (site) {
+    case 'github':
+      return <GitHubIcon />
+    case 'youtube':
+      return <YouTubeIcon />
+    case 'blog':
+      return <img src={Blog} />
+    default:
+      return <LanguageIcon />
+  }
+}
